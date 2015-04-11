7434faae03437338472ee28ba51f241332dd3cdf
Joseph Lee <ldp_0622@163.com>
2015-04-11 15:26:45 +0800

    fix cookie and addArticle

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -3,14 +3,23 @@ namespace Admin\Controller;
 use Think\Controller;
 
 class IndexController extends Controller {
+	//url list
+	private $index_url;
+	private $login_url;
+	private $addArticle_url;
+	private $blog_url;
 
+	function __construct() {
+		parent::__construct();
+		$this->index_url = U('Index/index');
+		$this->login_url = U('Index/login');
+		$this->addArticle_url = U('Index/addArticle');
+		$this->blog_url = U('Home/Index/index');
+	}
 	public function _before_index() {
 		//前置操作，在用户点击管理按钮后跳转到index页面
-		$cookie_value = cookie('jl_status');
-		if (empty($cookie_value)) {
-			//判断cookies,不存在，跳转到login action
-			$this->redirect('Index/login', '', 0, '');
-		}
+		$this->isCookie(__ACTION__);
+
 	}
 	public function index() {
 		$this->display();
@@ -25,7 +34,7 @@ class IndexController extends Controller {
 			if ($username == 'root' && $password == 'root') {
 				//正确，存cookie
 				cookie('status', md5('success'), array('expire' => 3600, 'prefix' => 'jl_'));
-				$this->redirect('Index/index', '', 0, '');
+				redirect(cookie('jl_return_url'), 0, '');
 			} else {
 				//DOM中添加警示框
 				$danger = '<div class="alert alert-danger alert-dismissible" role="alert">
@@ -38,10 +47,40 @@ class IndexController extends Controller {
 	}
 	public function login() {
 		//登录界面
-		$login_url = U('login');
-		$blog_url = U('Home/Index/index');
-		$this->assign('login_url', $login_url);
-		$this->assign('blog_url', $blog_url);
+		$cookie_value = cookie('jl_status');
+		if (!empty($cookie_value)) {
+			redirect($this->index_url, 0, '');
+		}
+		$this->assign('return_url', $this->return_url);
+		$this->assign('login_url', $this->login_url);
+		$this->assign('blog_url', $this->blog_url);
 		$this->display('login');
+	}
+	public function addArticle() {
+		$this->isCookie(__ACTION__);
+		if (IS_GET) {
+			$Category = M('Category');
+			$category_list = $Category->select();
+			$this->assign('category_list', $category_list);
+			$this->display();
+		} elseif (IS_POST) {
+			$Article = M('Article');
+			if ($Article->create()) {
+				$result = $Article->add(); // 写入数据到数据库
+				if ($result) {
+					// 如果主键是自动增长型 成功后返回值就是最新插入的值
+					$insertId = $result;
+				}
+			}
+		}
+	}
+	protected function isCookie($return_url) {
+		//用来判断cookie
+		$cookie_value = cookie('jl_status');
+		if (empty($cookie_value)) {
+			//给ruturn_url赋值，这样可以实现登陆后跳转到相应页面的情况
+			cookie('return_url', $return_url, array('expire' => 3600, 'prefix' => 'jl_'));
+			redirect($this->login_url, 0, '');
+		}
 	}
 }
